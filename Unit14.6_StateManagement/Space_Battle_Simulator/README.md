955c07ca17f69b4019e516ec6c92e9bc3fff7a6b
Aubrey <aholmes92@gmail.com>
2025-01-20 19:19:08 -0500

    Submitting Space Battle Simulator; State Management

---
+++ Unit14.6_StateManagement/Space_Battle_Simulator/README.md
@@ -0,0 +1,21 @@
+# State Management Exercise
+
+## Space Battle Simulator 🎇
+
+In this exercise, we will practice state management and event handling in React by creating a thrilling Space Battle Simulator. 💥
+
+Your mission is to create a simple interactive turn-based 🎰 space battle simulator where each fire ☄️ affects the health ❤️‍🩹 of both the player's and the enemy's spacecraft. The simulator will track the health points of both parties, dynamically updating after each attack based on random damage values. Additionally, the simulator is responsible for determining the battle's outcome—win, lose, or draw—based on the remaining health points. Moreover, when the battle ends, a restart feature will be available to initiate another round of interstellar combat. 🛸
+
+## Tasks 📋
+
+1. **Set Initial Health**: Begin your game by setting both the player's and the enemy's health to 100. These health values will change based on gameplay actions.
+2. **Track Game Status**: Monitor the state of the game, distinguishing between active play and the end of the game. Conclude the game with a status of win, loss, or draw, determined by comparing health points.
+3. **Gameplay Buttons**: Display a "Fire" button for players to use during the game. Replace it with a "Restart" button once the game concludes.
+4. **Attack Phase**: With each press of the "Fire" button:
+    - Determine random damage amounts for both the player and the enemy, sticking to a range provided via props. Ensure these props have default values.
+    - Deduct the appropriate damage from each party's health.
+    - Check if this round of attacks ends the game, either by reducing one party's health to 0 or both, and then update the game's status accordingly (win, loss, or draw).
+5. **End of Game**: Upon game conclusion:
+    - Display a specific message based on the outcome: a congratulatory note for winning, a message of commiseration for losing, or a remark on the stalemate for a draw.
+    - Hide the "Fire" button and show a "Restart" button to allow players to initiate a new game.
+6. **Game Reset**: Clicking the "Restart" button should revert all game elements to their starting conditions, ready for a new match.
---
+++ Unit14.6_StateManagement/Space_Battle_Simulator/src/App.jsx
@@ -0,0 +1,122 @@
+import { useState } from 'react' 
+import './App.css'
+
+function App({minDamage = 0, maxDamage = 50}) {
+  
+  // TASK 1 - Set Initial Health
+	const INITIAL_HEALTH = 100;
+	const INITIAL_GAME_STATUS = "Ready for War";
+
+	const [playerHealth, setPlayerHealth] = useState(INITIAL_HEALTH);
+	const [enemyHealth, setEnemyHealth] = useState(INITIAL_HEALTH);
+
+	const [gameStatus, setGameStatus] = useState(INITIAL_GAME_STATUS);
+
+	function handleAttack ()
+	{
+		const playerAttack = Math.floor(Math.random() * (maxDamage - minDamage + 1)) + minDamage;
+		const enemyAttack = Math.floor(Math.random() * (maxDamage - minDamage + 1)) + minDamage;
+
+		const newPlayerHealth = Math.max(playerHealth - enemyAttack, 0);
+		const newEnemyHealth = Math.max(enemyHealth - playerAttack, 0);
+
+		setPlayerHealth(newPlayerHealth);
+		setEnemyHealth(newEnemyHealth);
+
+		if (newPlayerHealth === 0 && newEnemyHealth === 0)
+		{
+			setGameStatus("draw");
+		}
+		else if (newEnemyHealth === 0)
+		{
+			setGameStatus("won");
+		}
+		else if (newPlayerHealth === 0)
+		{
+			setGameStatus("lost");
+		}
+	}
+
+	function handleRestart ()
+	{
+		setPlayerHealth(INITIAL_HEALTH);
+		setEnemyHealth(INITIAL_HEALTH);
+		setGameStatus(INITIAL_GAME_STATUS);
+	}
+
+	function renderGameStatusMessage ()
+	{
+		switch (gameStatus)
+		{
+			case "won":
+				return "Congratulations! 😎 You were VICTORIOUS!.";
+			case "lost":
+				return "Mission Failed. 😵️ Your you've been defeated.";
+			case "draw":
+				return "It's a draw! 🤝 Both spacecrafts have been neutralized.";
+			default:
+				return "CHARGE THE ENEMY!";
+		}
+	}
+
+	function renderHealth (health)
+	{
+		let emoji;
+
+		if (health === INITIAL_HEALTH)
+		{
+			emoji = "❤️";
+		}
+		else if (health === 0)
+		{
+			emoji = "💀";
+		}
+		else
+		{
+			emoji = "❤️‍🩹";
+		}
+
+		return `${health} ${emoji}`;
+	}
+
+	return (
+		<div className={"main-container"}>
+			<div className={"title-container"}>
+				<h1>Space Battle Simulator</h1>
+			</div>
+
+			<div className="game-container">
+				<div className={"player"}>
+					<p>Player Health: <span className={"score"}>{renderHealth(playerHealth)}</span></p>
+				</div>
+
+				{
+					gameStatus === "Ready for War"
+					&&
+					<div className={"attack"}>
+						<button onClick={handleAttack}>Fire!</button>
+					</div>
+				}
+
+				{
+					gameStatus !== "Ready for War"
+					&&
+					<div className={"restart"}>
+						<button onClick={handleRestart}>Restart?</button>
+					</div>
+				}
+
+				<div className={"enemy"}>
+					<p>Enemy Health: <span className={"score"}>{renderHealth(enemyHealth)}</span></p>
+				</div>
+			</div>
+
+			<div className={"message-container"}>
+				<p>{renderGameStatusMessage()}</p>
+			</div>
+		</div>
+	);
+
+}
+
+export default App
