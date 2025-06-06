0ff64b5c4161f6fc02604626dd6f7cfe32ef2791
Aubrey <aholmes92@gmail.com>
2025-06-05 20:48:12 -0400

    Adding middleware and logout functionality

--- Capstone/auth_nextjs/src/app/profile/page.tsx
+++ Capstone/auth_nextjs/src/app/profile/page.tsx
@@ -1,9 +1,30 @@
+"use client";
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+
 export default function ProfilePage() { 
+    
+    const router = useRouter()
+    const logout = async () => {
+        try {
+            await axios.get('/api/users/logout')
+            router.push('/login')
+        } catch (error:any) {
+            console.log(error.message);
+        }
+    }
+    
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Profile</h1>
             <hr />
             <p>Profile page</p>
+        <hr /> 
+        <button
+        onClick= {logout}
+        className="m-2 px-4 py-2 rounded bg-white-500 hover:bg-red-700 hover:text-white font-bold">Logout</button>
         </div>
     )
 }
